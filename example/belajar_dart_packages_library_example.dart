c86a92506bdf5f2fa9155f9470043fee9ac50380
Jonathan Deni <jonathan.jdk96@gmail.com>
2023-07-19 12:11:16 +0700

    chore(init): dart library

---
+++ example/belajar_dart_packages_library_example.dart
@@ -0,0 +1,6 @@
+import 'package:belajar_dart_packages_library/hello.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/hello.dart
@@ -0,0 +1,2 @@
+export 'src/say_hello.dart' show sayHello ;
+export 'src/customer.dart' show Customer, Category, Product;
